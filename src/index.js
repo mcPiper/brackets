f51ced83cc57dd586057c305a1248e84eece7ae1
mcPiper <66990552+mcPiper@users.noreply.github.com>
2021-08-28 10:28:50 +0300

    init: update

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
-module.exports = function check(str, bracketsConfig) {
+function check(str, bracketsConfig) {
+  console.log(str, bracketsConfig)
+  let stack = []
+  let bracketsDict = { }
+  let andBrackets = []
+  for (let i in bracketsConfig){
+    bracketsDict[bracketsConfig[i][0]] = bracketsConfig[i][1];
+    andBrackets.push(bracketsConfig[i][1])
+  }
+
+  for (let i=0; i<str.length; i++){
+    if (stack[stack.length-1] === str[i]){
+      stack.pop();
+    }
+    else if (bracketsDict[str[i]] ) {
+      stack.push(bracketsDict[str[i]]);
+    }
+    else if (andBrackets.indexOf(str[i]) > -1 ) {
+      return false;
+    }
+
+  }
+  return !stack[stack.length-1]
+}
+console.log(check("([[[[(({{{}}}(([](((((((())))||||||))))[[{{|{{}}|}}[[[[]]]]{{{{{}}}}}]]))))]]]]))()", [ [ '(', ')' ], [ '[', ']' ], [ '{', '}' ], [ '|', '|' ] ]))
+
+module.exports = (check)module.exports = function check(str, bracketsConfig) {
   // your solution
 }
